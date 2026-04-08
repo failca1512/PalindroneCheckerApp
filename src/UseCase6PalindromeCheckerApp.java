b1ebda64cd7781f54af37c334a75e44f639ab7b3
failca1512 <karpagamarul200@gmail.com>
2026-04-08 08:59:32 +0530

    Added UC7 - Deque based optimized palindrome cheker

--- src/UseCase6PalindromeCheckerApp.java
+++ src/UseCase6PalindromeCheckerApp.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class UseCase6PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "civic";
+
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        // Enqueue & Push
+        for (char c : input.toCharArray()) {
+            queue.add(c);     // FIFO
+            stack.push(c);    // LIFO
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare dequeue vs pop
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
 }
