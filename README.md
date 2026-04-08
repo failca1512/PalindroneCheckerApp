8f899048dae53cef236f109f1fd6162887e29337
failca1512 <karpagamarul200@gmail.com>
2026-04-08 10:22:26 +0530

    Added UC12 - Stratergy pattern for dynamic palindrome checking

--- README.md
+++ README.md
@@ -192,3 +192,21 @@ Encapsulate palindrome logic in a class.
 
 ### Data Structure:
 Internal (String / Array)
+
+## UC12: Strategy Pattern for Palindrome Algorithms
+
+### Goal:
+Choose a palindrome algorithm dynamically.
+
+### Flow:
+- Define PalindromeStrategy interface
+- Implement StackStrategy, DequeStrategy
+- Inject strategy at runtime
+
+### Key Concepts:
+- Interface
+- Polymorphism
+- Strategy Pattern
+
+### Data Structure:
+Varies per strategy
---
+++ src/UseCase12PalindromeCheckerApp.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+        Stack<Character> stack = new Stack<>();
+
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input.toCharArray()) {
+            deque.add(c);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+public class UseCase12PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+
+        PalindromeStrategy strategy;
+
+
+        strategy = new StackStrategy();
+
+
+        boolean result = strategy.check(input);
+
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome?: " + result);
+    }
+}
